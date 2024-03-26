40d327cbed1b9b47466e8d1249d255dc54776887
Ermano Arruda <ermano.arruda@gmail.com>
2024-03-26 14:37:51 +0000

    Update no_deadband-launch.py
    update executable node name

--- spacenav/launch/no_deadband-launch.py
+++ spacenav/launch/no_deadband-launch.py
@@ -31,7 +31,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(package='spacenav', executable='spacenav',
+        Node(package='spacenav', executable='spacenav_node',
              name='spacenav', namespace='', output='screen',
              parameters=[{'zero_when_static': False,
                           'static_trans_deadband': 0.0,
